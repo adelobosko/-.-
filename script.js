76d6af0d86873e146a742d28863593da19aead1a
adelobosko <56083544+adelobosko@users.noreply.github.com>
2020-04-15 00:20:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,25 @@
 'use strict';
 
 var trelloCookieName = 'trelloAccessToken';
-var trelloAccessUrl = 'https://trello.com/1/authorize?expiration=never&name=Ранобэ.рф&scope=read,write&response_type=token&key=7d0d6fa3ed4fe0b723201010a2b0dc19&return_url=';
+var trelloCardIdCookieName = 'trelloCardId';
+var trelloKey = '7d0d6fa3ed4fe0b723201010a2b0dc19';
+var trelloAccessUrl = 'https://trello.com/1/authorize?expiration=never&name=Ранобэ.рф&scope=read,write&response_type=token&key='+trelloKey+'&return_url=';
 var trelloToken = '';
+var trelloCardId = '';
 var domainName = 'xn--80ac9aeh6f.xn--p1ai';
+
+var trelloIconId = 'trelloIcon';
+var trelloMenuId = 'trelloMenu';
+
+
+const TrelloStatus = {
+  'NOT_ALLOWED': 0,
+  'ALLOWED_NOT_CONFIGURED': 1,
+  'ALLOWED_CONFIGURED': 2
+}
+
+var trelloStatus = TrelloStatus.NOT_ALLOWED;
+
 
 function createCssLink(id, href){
 	var cssId = id;
@@ -81,29 +97,155 @@ function redirectToTrello(){
 }
 
 
-function createTrelloIcon(accessAllowed){
-  var src = accessAllowed ? 'https://github.com/adelobosko/images/raw/master/trelloChecked.png' : 'https://github.com/adelobosko/images/raw/master/trelloUnchecked.png';
-  var iconId = 'trelloScript';
-  var imgHtml = '<img id="'+iconId+'" style="height:48px;cursor:pointer;" src="'+src+'"></img>';
-  var menuHtml = '<li class="MobileMenu__item MobileMenu__item_chapters"><div class="Chapters MobileMenu__part MobileMenu__part_disabled"><div class="PopupButton PopupButton_disabled Chapters__toggler" data-id="chapters_popup_mobile" tabindex="0" role="button">'+imgHtml+'</div></div></li>';
- 
-  var imgElement = $('#'+iconId);
-  if(imgElement.length > 0){
-    imgElement.attr('src',src);    
+function addComment(cardid, text){
+  var xhr = new XMLHttpRequest();
+  var body = 'text=' + encodeURIComponent(text)+'&key='+trelloKey+'&token='+trelloToken;
+  
+  xhr.open("POST", 'https://api.trello.com/1/cards/'+cardid+'/actions/comments', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(body);
+}
+
+
+
+function getCards(boardId){
+  var xhr = new XMLHttpRequest();
+  var body = 'key='+trelloKey+'&token='+trelloToken;
+  xhr.open("GET", 'https://api.trello.com/1/boards/'+boardId+'/cards?' + body, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var cards = JSON.parse(xhr.responseText);
+      var cardsHtml = '';
+      for(var i = 0; i < cards.length; i++){
+        var card = cards[i];
+        cardsHtml += '<option value="'+card.id+'">'+card.name+'</option>';
+      }
+      
+      $('#trelloCardSelect').html(cardsHtml);
+    }
+  }
+  xhr.send();
+}
+
+
+var menu = {
+	'create': function (boards) {
+    var logoutHtml = '<i id="trelloLogout" style="padding-left:5px;float:left;color:red;cursor:pointer;" class="fas fa-sign-out-alt"></i>';
+    var cardIconColor = trelloCardId ? 'green' : 'white';
+    var cardHtml = '<i id="trelloCard" style="padding-left:10px;float:left;color:'+cardIconColor+';cursor:pointer;" class="far fa-credit-card"></i>';
+    var menuHtml = '<div id="'+trelloMenuId+'" style="display:none;width:120px;position:fixed;bottom:64px;">'+logoutHtml+cardHtml+'</div>';
+    
+    
+    var boardsHtml = 'Select board:<br><select id="trelloBoardSelect" value="-1">';
+    var cardsHtml = '<br>Select card:<br><select id="trelloCardSelect" value="-2"></select>';
+    for(var i = 0; i < boards.length; i++){
+      var board = boards[i];
+      console.log('board', board);
+      if(board.closed === false){
+        boardsHtml += '<option value="'+board.id+'">'+board.name+'</option>';
+      }
+    }
+    boardsHtml += '</select>';
+    
+    var selectCardHtml = '<div id="trelloSelectCard" style="color:green;display:none;width:80%;height:90%;position:fixed;top:10px;left:10px;">'+boardsHtml+cardsHtml+'</div>';
+    
+    if($('#'+trelloMenuId).length > 0){
+      $('#'+trelloMenuId).remove();
+    }
+    
+    $('#'+trelloIconId).parent().append(menuHtml);
+    
+    if($('#trelloSelectCard').length > 0){
+      $('#trelloSelectCard').remove();
+    }
+    
+    $('#root').html($('#root').html()+selectCardHtml);
+    
+    $('#trelloBoardSelect').unbind();
+    $('#trelloBoardSelect').on('change', function() {
+      getCards(this.value);
+    });
+    
+    $('#trelloCardSelect').unbind();
+    $('#trelloCardSelect').on('change', function() {      
+      setCookie(trelloCardIdCookieName, this.value, {domain: domainName, 'max-age': 31536000});
+      trelloCardId = this.value;
+      
+    });
+    $('#trelloCard').unbind();
+    $('#trelloCard').click(function(){
+      $('#trelloSelectCard').css('display', 'block');
+    });
+
+    $('#trelloLogout').unbind();
+    $('#trelloLogout').click(function(){
+     if(confirm('Do you want to delete access token?')){
+        clearCookie(trelloCookieName, domainName);
+        clearCookie(trelloCardIdCookieName, domainName);
+        createTrelloIcon();
+      }
+    });
+    this.isCreated = true;
+    this.showOrHide();
+  },
+	'showOrHide': function(force){
+    if($('#'+trelloMenuId).length > 0){
+      this.isShown = force || !this.isShown;
+      $('#'+trelloMenuId).css('display', this.isShown ? 'block' : 'none');
+      return;
+    }
+  },
+  'isShown': false,
+  'isCreated': false
+};
+
+
+function getBoards(){
+  var xhr = new XMLHttpRequest();
+  var body = 'key='+trelloKey+'&token='+trelloToken;
+  xhr.open("GET", 'https://api.trello.com/1/members/me/boards?' + body, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var boards = JSON.parse(xhr.responseText);
+      menu.create(boards);
+    }
+  }
+  xhr.send();
+}
+
+
+function createTrelloIcon(){
+  var src = 'https://github.com/adelobosko/images/raw/master/trelloUnchecked.png';
+  
+  if(trelloStatus === TrelloStatus.ALLOWED_NOT_CONFIGURED){
+    src = 'https://raw.githubusercontent.com/adelobosko/images/master/trelloConfigure.png';
+  }
+  else if(trelloStatus === TrelloStatus.ALLOWED_CONFIGURED){
+    src = 'https://github.com/adelobosko/images/raw/master/trelloChecked.png';
+  }
+  
+
+  if($('#'+trelloIconId).length > 0){
+    $('#'+trelloIconId).attr('src',src);    
   }
   else {
+    var imgHtml = '<img id="'+trelloIconId+'" style="height:48px;cursor:pointer;" src="'+src+'"></img>';
+    var menuHtml = '<li class="MobileMenu__item MobileMenu__item_chapters"><div class="Chapters MobileMenu__part MobileMenu__part_disabled"><div class="PopupButton PopupButton_disabled Chapters__toggler" data-id="chapters_popup_mobile" tabindex="0" role="button">'+imgHtml+'</div></div></li>';
+ 
     $('ul.MobileMenu__list').append(menuHtml);
   }
   
-  imgElement.unbind();    
-  imgElement.click(function(){
-    if(accessAllowed){
-      if(confirm('Do you want to delete access token?')){        
-        clearCookie(trelloCookieName, domainName);
-        createTrelloIcon(false);
-      }
+  $('#'+trelloIconId).unbind();    
+  $('#'+trelloIconId).click(function(){
+    if(trelloStatus === TrelloStatus.ALLOWED_CONFIGURED){
+      getBoards();
     }
-    else {
+    else if(trelloStatus === TrelloStatus.ALLOWED_NOT_CONFIGURED){
+      getBoards();
+    }
+    else if(trelloStatus === TrelloStatus.NOT_ALLOWED) {
       redirectToTrello();
     }
   });
@@ -123,16 +265,33 @@ function checkUrl(){
   }
   var token = url.substring(startIndex+urlStartText.length);
   clearCookie(trelloCookieName, domainName);
-  setCookie(trelloCookieName, token, {domain: domainName, 'max-age': 31536000});    
+  setCookie(trelloCookieName, token, {domain: domainName, 'max-age': 31536000});
+}
+
+
+function checkTrelloStatus(){
+  trelloToken = getCookie(trelloCookieName);
+  trelloCardId = getCookie(trelloCardIdCookieName);
+  
+  var status = TrelloStatus.NOT_ALLOWED;
+  if(trelloToken){
+    status = TrelloStatus.ALLOWED_NOT_CONFIGURED;
+    
+    if(trelloCardId){
+      status = TrelloStatus.ALLOWED_CONFIGURED;
+    }
+  }
+  
+  return status;
 }
 
 
 function checkStatus(){
   checkUrl();
-  trelloToken = getCookie(trelloCookieName);
-  createTrelloIcon(trelloToken);
+  trelloStatus = checkTrelloStatus();
+  createTrelloIcon();
   
-  setTimeout(checkStatus, 10000);
+  setTimeout(checkStatus, 3000);
 }
 
 
@@ -141,16 +300,6 @@ function initialize(){
 
   setTimeout(checkStatus, 3000);
 }
-
-
-var menu = {
-	'script': {
-		enabled: true
-	},
-	'playSound': {
-		enabled: GM_getValue('playSound', true)
-	}
-};
 
 
 
